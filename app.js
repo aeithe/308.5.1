8f99ba79ff4babe5f15e3fe80cdf1263e049e2e5
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-05-20 12:03:53 -0400

    did some of part 1 ;-;

--- app.js
+++ app.js
@@ -21,24 +21,24 @@
 
  const numerics = [1, 2, 3, 4, 5];
 // const sumOfNums = [];
-function addition(string, operator){
-for (let i = 0; i <= string.length; i++){
-    if (operator ='+'){
-       return string[i]+string[i]
-    }
-    else if (operator = '-'){
-       return string[i]-string[i]
-    }
-    else if (operator = '*'){
-       return string[i]*string[i]
-    }
-    else if (operator = '/'){
-        return string[i]/string[i]
-    };
-};
-};
+// function addition(string, operator){
+// for (let i = 0; i <= string.length; i++){
+//     if (operator ='+'){
+//        return string[i]+string[i]
+//     }
+//     else if (operator = '-'){
+//        return string[i]-string[i]
+//     }
+//     else if (operator = '*'){
+//        return string[i]*string[i]
+//     }
+//     else if (operator = '/'){
+//         return string[i]/string[i]
+//     };
+// };
+// };
 
-console.log(addition(numerics, '+'));
+// console.log(addition(numerics, '+'));
     //let sum = currentValue[i]+ initialValue, index = 0;
     //let initialValue = 0;
 //want to create a function that does any kind of math upon a given array with a given operator 
@@ -50,3 +50,34 @@ console.log(addition(numerics, '+'));
 // if(operator = +){
     
 //}
+
+function sumsOfNumbers (array, combine, start){
+    let current = start; 
+    for (let element of array){
+        current = combine(current, element);
+    
+    }
+    return current;
+}
+const sumOfNums = sumsOfNumbers(numerics, (a, b) => a + b, 0);
+
+console.log(sumOfNums);
+
+console.log(sumOfNums/numerics.length);
+
+const arrOfStrings = ["hello", "how are you?", "hey", "what's up?"];
+
+const longestInString = arrOfStrings.reduce(function (a,b){
+    return a.length > b.length ? a:b
+}, "");
+console.log(longestInString);
+
+const arrOfStringsAndNum = ["hello", "how are you?", "hey", "what's up?", 3];
+
+const longerThanThree = arrOfStringsAndNum.reduce(function (arr){
+    tempArr = [];
+    
+}, "");
+console.log(longerThanThree);
+
+// console.log(arrOfStrings.localeCompare(3));
